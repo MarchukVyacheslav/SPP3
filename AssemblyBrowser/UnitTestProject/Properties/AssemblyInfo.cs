b32689c85deca15d02bf53446b1fc46a1a425026
MarchukVyacheslav <marchukvyacheslav@bk.ru>
2021-10-26 21:06:12 +0300

    += UnitTests

---
+++ AssemblyBrowser/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a1b5c4d-469a-4904-bc56-1b03e18502f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssemblyBrowser/UnitTestProject/UnitTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyBrowserLib;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTests
+    {
+        private readonly AssemblyBrowser _browser = new AssemblyBrowser();
+        private const string TestDirectory = "E:\\Documents\\University\\Марчук 951004\\3 курс\\5 семестр\\СПП\\Лабораторные работы\\SPP3\\AssemblyBrowser\\View\\bin\\Debug\\";
+
+        [TestMethod]
+        public void DllBrowserWorkFinishedCorrectly()
+        {
+            _browser.GetAssemblyInfo(TestDirectory + "AssemblyBrowserLib.dll");
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ExeBrowserWorkFinishedCorrectly()
+        {
+            _browser.GetAssemblyInfo(TestDirectory + "View.exe");
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void NameSpaceIsCorrect()
+        {
+            var assemblyInfo = _browser.GetAssemblyInfo(TestDirectory + "AssemblyBrowserLib.dll");
+            ;
+            Assert.IsTrue(assemblyInfo[0].Signature.Equals("AssemblyBrowserLib"));
+            Assert.IsTrue(assemblyInfo[1].Signature.Equals("AssemblyBrowserLib.Format"));
+        }
+
+        [TestMethod]
+        public void MethodNotNull()
+        {
+            var assemblyInfo = _browser.GetAssemblyInfo(TestDirectory + "View.exe");
+            ;
+            Assert.IsNotNull(assemblyInfo[1].Members);
+        }
+    }
+}
