f9be5be076186300a812f1f92fe446beee8c5e79
MarchukVyacheslav <marchukvyacheslav@bk.ru>
2021-10-26 10:41:24 +0300

    += class, constructor, field, method, properties format for print

--- AssemblyBrowser/AssemblyBrowserLib/Format/FieldFormatter.cs
+++ AssemblyBrowser/AssemblyBrowserLib/Format/FieldFormatter.cs
@@ -10,9 +10,71 @@ namespace AssemblyBrowserLib.Format
     {
         public static string Format(FieldInfo fieldInfo)
         {
-            var result = "";
-
-            return result;
+            return string.Join(" ", GetTypeAccessorModifiers(fieldInfo), GetTypeModifiers(fieldInfo), GetType(fieldInfo), fieldInfo.Name);
         }
-    }
+
+        private static string GetTypeAccessorModifiers(FieldInfo filedInfo)
+        {
+            // new 
+            if (filedInfo.IsPublic)
+                return "public";
+            if (filedInfo.IsPrivate)
+                return "private";
+            if (filedInfo.IsFamily)
+                return "protected";
+            if (filedInfo.IsAssembly)
+                return "internal";
+            if (filedInfo.IsFamilyOrAssembly)
+                return "protected internal";
+
+            return "";
+        }
+
+        private static string GetTypeModifiers(FieldInfo fieldInfo)
+        {
+            return fieldInfo.IsStatic ? "static" : "";
+        }
+
+        private static string GetType(FieldInfo fieldInfo)
+        {
+            if (fieldInfo.FieldType.IsGenericType) return GetGenericType(fieldInfo.FieldType);
+            return fieldInfo.FieldType.Name;
+        }
+
+        private static string GetGenericType(Type parameter)
+        {
+
+            var stringBuilder = new StringBuilder(Regex.Replace(parameter.Name, "`[0-9]+$", ""));
+
+            stringBuilder.Append("<");
+            if (parameter.IsGenericType)
+            {
+                stringBuilder.Append(GetGenericArgumentsType(parameter.GenericTypeArguments));
+            }
+            stringBuilder.Append(">");
+
+            return stringBuilder.ToString();
+        }
+
+
+        private static string GetGenericArgumentsType(IEnumerable<Type> arguments)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var argument in arguments)
+            {
+                if (argument.IsGenericType)
+                {
+                    stringBuilder.Append(GetGenericType(argument));
+                }
+                else stringBuilder.Append(argument);
+
+                stringBuilder.Append(", ");
+            }
+
+            stringBuilder.Remove(stringBuilder.Length - 2, 2);
+
+            return stringBuilder.ToString();
+        }
+        }
 }
